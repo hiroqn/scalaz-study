949b66ab03e4508af142e623435b730a36800617
hiroqn <hiroqn1008@gmail.com>
2016-05-18 14:45:12 +0900

    😅

---
+++ src/p2-2.scala
@@ -0,0 +1,16 @@
+def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+  as match {
+    case Array() => true
+    case Array(_) => true
+    case Array(a0, a1, _) => {
+      def f(a0: A, a1: A, rest: Array[A]): Boolean = {
+        if (ordered(a0, a1)) {
+          f(a1, rest.head, rest.tail)
+        } else {
+          false
+        }
+      }
+      f(as(0), as(1), as.tail.tail)
+    }
+  }
+}
