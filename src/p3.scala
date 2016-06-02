60ce49ffa383ac7196620c7905c86ccbf3b792f6
hiroqn <hiroqn1008@gmail.com>
2016-06-02 19:27:00 +0900

    😂😭

--- src/p3.scala
+++ src/p3.scala
@@ -6,11 +6,6 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
 
-  def sum(ints: List[Int]): Int = ints match {
-    case Nil => 0
-    case Cons(x, xs) => x + sum(xs)
-  }
-
   def tail[A](x: List[A]): List[A] = x match {
     case Nil => Nil
     case Cons(_, xs: List[A]) => xs
@@ -21,11 +16,11 @@ object List {
     case Cons(_, xs: List[A]) => Cons(another, xs)
   }
 
-  def drop[A](l: List[A], n: Int): List[A] = drop(tail(l), n - 1)
+  def drop[A](l: List[A], n: Int): List[A] = if (n == 0) l else drop(tail(l), n - 1)
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Nil => Nil
-    case Cons(x: A, xs: List[A]) =>
+    case Cons(x, xs) =>
       if (f(x))
         dropWhile(xs, f)
       else
@@ -41,16 +36,45 @@ object List {
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+
+  def foldLeft2[A, B](as: List[A], z: B)(f: (B, A) => B): B =
+    foldRight2(as, (b: B) => b)((a: A, bf: B => B) => (bb: B) => bf(f(bb, a)))(z)
+
+  def foldRight2[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft2(as, (b: B) => b)((bf: B => B, a:A) => (bb: B) => bf(f(a, bb)))(z)
+
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+  def product(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
+
+  def length[A](as: List[A]): Int = foldRight(as, 0)((_, x) => x + 1)
+
+  def sum[A](as: List[Int]) = foldLeft(as, 0)(_ + _)
+
+  def reverse[A](as: List[A]) = foldLeft2(as, Nil: List[A])((x, xs) => Cons(xs, x))
+
+  def append[A](as: List[A], ass: List[A]): List[A] = foldRight(as, ass)(Cons(_, _))
+
+  def flatten[A](ass: List[List[A]]): List[A] = foldRight2(ass, Nil: List[A])(append)
 }
 
-val x = List(1, 2, 3, 4, 5) match {
-  case Cons(x, Cons(2, Cons(4, _))) => x
-  case Nil => 42
-  case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-  case Cons(h, t) => h + List.sum(t)
-  case _ => 101
+println(List.length(List(1, 2, 3, 4)))
 
-}
+println(List.reverse(List(1, 2, 3, 4)))
 
-print(List.tail(List(1, 2, 3, 4, 5)))
+println(List.append(List(1, 2, 3, 4), List(1, 2, 3, 4)))
 
+println(List.flatten(List(List(1, 2, 3, 4), List(1, 2, 3, 4), List(1, 2, 3, 4))))
